78feeb4cd7eec13db3ff8310be07aba552c6956f
Uzgur <uzbmv2@gmail.com>
2023-11-15 18:16:13 +0500

    types are ready

---
+++ barmen_bakcend/src/types.ts
@@ -0,0 +1,30 @@
+export namespace IDrinks {
+  export interface ICategory {
+    id: number;
+    name: string;
+    drinks: IDrink[];
+  }
+
+  export interface IDrink {
+    id: number;
+    image: string;
+    size: string;
+    name: string;
+    type: string;
+    brand: string;
+    price: number;
+  }
+}
+
+export namespace IUser {
+  export interface IUser {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+  }
+
+  export interface IAdmin extends IUser {
+    isAdmin: boolean;
+  }
+}
