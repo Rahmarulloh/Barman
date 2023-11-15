c149344db4d59c18e94924d0908203913e91db0a
Uzgur <uzbmv2@gmail.com>
2023-11-16 01:12:31 +0500

    Singel and Listing requests finished by Samandar aka

--- barmen_bakcend/src/users/admin.js
+++ barmen_bakcend/src/users/admin.js
@@ -71,3 +71,141 @@ export function putDrink(req, res) {
     return;
   }
 }
+
+export function addCocktail(req, res) {
+  if (admin.login === "admin444@gmail.com" && admin.password === "111444") {
+    const cocktail = {
+      id: faker.string.uuid(),
+      ...req.body,
+    };
+
+    drinkCategories[0].drinks.unshift(cocktail);
+    res.send({
+      data: cocktail,
+      message: `Successfully created cocktail`,
+      success: true,
+    });
+  } else {
+    return;
+  }
+}
+
+export function deleteCocktail(req, res) {
+  if (admin.login === "admin444@gmail.com" && admin.password === "111444") {
+    const { cocktailID } = req.params;
+
+    const cocktailsIdx = drinkCategories[0].drinks.findIndex(
+      (cocktail) => cocktail.id === cocktailID
+    );
+
+    if (cocktailsIdx === -1)
+      return res.status(404).send({
+        data: null,
+        message: `Not found cocktail with id ${cocktailID}`,
+        success: false,
+      });
+
+    const [deletedCocktail] = drinkCategories[0].drinks.splice(cocktailsIdx, 1);
+    res.send({ data: deletedCocktail, message: null, success: true });
+  } else {
+    return;
+  }
+}
+
+export function putCocktail(req, res) {
+  if (admin.login === "admin444@gmail.com" && admin.password === "111444") {
+    const { cocktailID } = req.params;
+
+    const cocktailsIdx = drinkCategories[0].drinks.findIndex(
+      (cocktail) => cocktail.id === cocktailID
+    );
+
+    if (cocktailsIdx === -1)
+      return res.status(404).send({
+        data: null,
+        message: `Not found cocktail with id ${cocktailID}`,
+        success: false,
+      });
+
+    drinkCategories[0].drinks[cocktailsIdx] = {
+      ...drinkCategories[0].drinks[cocktailsIdx],
+      ...req.body,
+    };
+    res.send({
+      data: drinkCategories[0].drinks[cocktailsIdx],
+      message: "Successfully updated cocktail",
+      success: true,
+    });
+  } else {
+    return;
+  }
+}
+export function addEnergetic(req, res) {
+  if (admin.login === "admin444@gmail.com" && admin.password === "111444") {
+    const energetic = {
+      id: faker.string.uuid(),
+      ...req.body,
+    };
+
+    drinkCategories[1].drinks.unshift(energetic);
+    res.send({
+      data: energetic,
+      message: `Successfully created energetic drink`,
+      success: true,
+    });
+  } else {
+    return;
+  }
+}
+export function deleteEnergetic(req, res) {
+  if (admin.login === "admin444@gmail.com" && admin.password === "111444") {
+    const { energeticID } = req.params;
+
+    const energeticsIdx = drinkCategories[1].drinks.findIndex(
+      (energetic) => energetic.id === energeticID
+    );
+
+    if (energeticsIdx === -1)
+      return res.status(404).send({
+        data: null,
+        message: `Not found energetic drink with id ${energeticID}`,
+        success: false,
+      });
+
+    const [deletedEnergetic] = drinkCategories[1].drinks.splice(
+      energeticsIdx,
+      1
+    );
+    res.send({ data: deletedEnergetic, message: null, success: true });
+  } else {
+    return;
+  }
+}
+export function putEnergetic(req, res) {
+  if (admin.login === "admin444@gmail.com" && admin.password === "111444") {
+    const { energeticID } = req.params;
+
+    const energeticsIdx = drinkCategories[1].drinks.findIndex(
+      (energetic) => energetic.id === energeticID
+    );
+
+    if (energeticsIdx === -1)
+      return res.status(404).send({
+        data: null,
+        message: `Not found energetic drink with id ${energeticID}`,
+        success: false,
+      });
+
+    drinkCategories[1].drinks[energeticsIdx] = {
+      ...drinkCategories[1].drinks[energeticsIdx],
+      ...req.body,
+    };
+    res.send({
+      data: drinkCategories[1].drinks[energeticsIdx],
+      message: "Successfully updated energetic drink",
+      success: true,
+    });
+  } else {
+    return;
+  }
+}
