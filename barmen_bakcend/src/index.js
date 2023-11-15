b2db6be1f3fddc37ca8123c5fefda9af8f72cbba
rmuhammadjon <rixsiyevmuhammad04@gmail.com>
2023-11-15 22:33:34 +0500

    added cods for admin

--- barmen_bakcend/src/index.js
+++ barmen_bakcend/src/index.js
@@ -1,6 +1,7 @@
 // src/index.ts
 import express from "express";
 import { Energetics, drinkCategories } from "./constants/constant.js";
+import { addDrink, deleteDrink, putDrink } from "./users/admin.js";
 
 const app = express();
 const port = 4000;
@@ -9,6 +10,9 @@ app.get("/api/drinks", (req, res) => {
   res.send(drinkCategories);
 });
 
+app.post("/api/drinks",addDrink);
+app.delete("api/drinks/:drinkID",deleteDrink);
+app.put("api/drinks/:drinkID",putDrink);
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
